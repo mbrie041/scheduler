4be25cfddd3eca3f2ba186ac07017d3c1bcbbc35
mbrie041 <mbrie041@gmail.com>
2021-04-15 20:11:08 -0400

    Managing State

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React, { useState, setResults, useEffect } from "react";
 import DayList from "components/daylist";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
-import axios from "axios"
+import axios from "axios";
 
 const appointments = [
   {
@@ -18,8 +18,8 @@ const appointments = [
         id: 1,
         name: "Sylvia Palmer",
         avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
+      },
+    },
   },
   {
     id: 3,
@@ -33,9 +33,9 @@ const appointments = [
       interviewer: {
         id: 2,
         name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png" ,
-      }
-    }
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      },
+    },
   },
   {
     id: 5,
@@ -45,35 +45,36 @@ const appointments = [
       interviewer: {
         id: 3,
         name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png" ,
-      }
-    }
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      },
+    },
   },
   {
     id: 6,
     time: "5pm",
-  }
+  },
 ];
 
-
-
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+  });
+  const setDay = (day) => setState({ ...state, day });
+  const setDays = (days) => setState((prev) => ({ ...prev, days }));
 
   useEffect(() => {
     const testURL = `/api/days`;
-    axios.get(testURL).then(response => {
-      setDays([...response.data])
+    axios.get(testURL).then((response) => {
+      setDays([...response.data]);
       console.log(response.data);
     });
-  }, [])
+  }, []);
 
-  const interatedAppointments = appointments.map(appointment =>
-    <Appointment 
-    key={appointment.id}
-    {...appointment}
-    />)
+  const interatedAppointments = appointments.map((appointment) => (
+    <Appointment key={appointment.id} {...appointment} />
+  ));
 
   return (
     <main className="layout">
@@ -85,11 +86,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList
-            days={days}
-            day={day}
-            setDay={setDay}
-          />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -97,9 +94,7 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule">
-        {interatedAppointments}
-      </section>
+      <section className="schedule">{interatedAppointments}</section>
     </main>
   );
 }
