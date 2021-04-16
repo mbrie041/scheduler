c3ee95f8c9000ce56389f1dd93832aac78501619
mbrie041 <mbrie041@gmail.com>
2021-04-15 22:25:02 -0400

    Retrieving Interviewer Data

--- src/components/helpers/selectors.js
+++ src/components/helpers/selectors.js
@@ -1,11 +1,24 @@
-export function getAppointmentsForDay(state, day) {
-
-  const foundDay = state.days.find(givenObject => givenObject.name === day);
+ const getAppointmentsForDay = (state, day) => {
+  const foundDay = state.days.find((givenObject) => givenObject.name === day);
   if (foundDay) {
-      const foundAppointments = foundDay.appointments.map(id => state.appointments[id])
-      return foundAppointments
-    } else {
-      return [];
-    }
-
+    const foundAppointments = foundDay.appointments.map(
+      (id) => state.appointments[id]
+    );
+    return foundAppointments;
+  } else {
+    return [];
+  }
 }
+
+const getInterview = (state, interview) => {
+  if (!interview) {
+    return null;
+  }
+  const interviewObj = {
+    ...interview,
+    interviewer: { ...state.interviewers[interview.interviewer] },
+  };
+  return interviewObj;
+};
+
+export { getAppointmentsForDay,getInterview}
