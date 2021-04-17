1364ea89aedfce4da7f7267024b47c46fc81e5d3
mbrie041 <mbrie041@gmail.com>
2021-04-16 21:23:20 -0400

    Creating Appointments

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,44 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
-  const [interviewer, setInterviewer] = useState(props.interviewer || null)
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
   const reset = () => {
     setName("");
     setInterviewer(null);
-  }
+  };
 
   const cancel = () => {
     props.onCancel();
     reset();
-  }
+  };
 
-return(
-<main className="appointment__card appointment__card--create">
-  <section className="appointment__card-left">
-    <form autoComplete="off">
-      <input
-        className="appointment__create-input text--semi-bold"
-        name="name"
-        value={name}
-        type="text"
-        placeholder="Enter Student Name"
-        onChange={(event)=> setName(event.target.value)}
-        /*
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            value={name}
+            type="text"
+            placeholder="Enter Student Name"
+            onChange={(event) => setName(event.target.value)}
+            /*
           This must be a controlled component
         */
-      />
-    </form>
-    <InterviewerList interviewers={props.interviewers} interviewer={interviewer} setInterviewer={setInterviewer} />
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <Button onClick={cancel} danger>Cancel</Button>
-      <Button confirm>Save</Button>
-    </section>
-  </section>
-</main>
-)}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          interviewer={interviewer}
+          setInterviewer={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button onClick={cancel} danger>
+            Cancel
+          </Button>
+          <Button onClick={()=> props.onSave(name, interviewer)} confirm>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
+}
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,18 +1,43 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, setResults } from "react";
 import "./styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import Form from "components/Appointment/Form";
+import Status from "components/Appointment/status";
 import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
+  const { interview } = props
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
-   const { mode, transition, back } = useVisualMode(
+  const SAVING = "SAVING";
+  const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING)
+    props
+    .bookInterview(props.id, interview)
+    .then(() => transition(SHOW))
+    .catch((error)=> console.log(error))
+   
+  }
+  useEffect(() => {
+    if (props.interview && mode === EMPTY) {
+      transition(SHOW);
+    }
+    if (props.interview === null && mode === SHOW) {
+      transition(EMPTY);
+    }
+  }, [props.interview, transition, mode]);
+
 
   return (
     <Fragment>
@@ -29,8 +54,10 @@ export default function Appointment(props) {
           <Form
             interviewers={props.interviewers}
             onCancel={back}
+            onSave={save}
           />
         )}
+        {mode === SAVING && (<Status message="Saving"/>)}
       </article>
     </Fragment>
   );
